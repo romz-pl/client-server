f0794fbe464109097fc0f593474a4266ea682ed9
Zbigniew Romanowski <romz@wp.pl>
2017-11-18 18:16:58 +0100

    More white spaces

--- src/tcp_server.c
+++ src/tcp_server.c
@@ -11,53 +11,67 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-void dostuff(int); /* function prototype */
-void error(const char *msg)
+void dostuff( int ); /* function prototype */
+void error( const char *msg )
 {
-    perror(msg);
-    exit(1);
+    perror( msg );
+    exit( 1 );
 }
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, pid;
-     socklen_t clilen;
-     struct sockaddr_in serv_addr, cli_addr;
+    int sockfd, newsockfd, portno, pid;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+    if( argc < 2 )
+    {
+        fprintf( stderr, "ERROR, no port provided\n" );
+        exit(1);
+    }
+    sockfd = socket( AF_INET, SOCK_STREAM, 0 );
+    if( sockfd < 0 )
+    { 
         error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     while (1) {
-         newsockfd = accept(sockfd, 
-               (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0) 
-             error("ERROR on accept");
-         pid = fork();
-         if (pid < 0)
-             error("ERROR on fork");
-         if (pid == 0)  {
-             close(sockfd);
-             dostuff(newsockfd);
-             exit(0);
-         }
-         else close(newsockfd);
-     } /* end of while */
-     close(sockfd);
-     return 0; /* we never get here */
+    }
+    bzero( (char *)&serv_addr, sizeof( serv_addr ) );
+    portno = atoi( argv[ 1 ] );
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons( portno );
+    if( bind( sockfd, (struct sockaddr *)&serv_addr, sizeof( serv_addr ) ) < 0 )
+    { 
+        error( "ERROR on binding" );
+    }
+    listen( sockfd, 5 );
+    clilen = sizeof( cli_addr );
+
+    while( 1 )
+    {
+        newsockfd = accept( sockfd, (struct sockaddr *)&cli_addr, &clilen );
+        if( newsockfd < 0 )
+        { 
+            error( "ERROR on accept" );
+        }
+        pid = fork();
+        if( pid < 0 )
+        {
+            error( "ERROR on fork" );
+        }
+        if( pid == 0 )
+        {
+            close( sockfd );
+            dostuff( newsockfd );
+            exit( 0 );
+        }
+        else
+        {
+            close( newsockfd );
+        }
+    } /* end of while */
+
+    close( sockfd );
+    return 0; /* we never get here */
 }
 
 /******** DOSTUFF() *********************
@@ -65,15 +79,22 @@ int main(int argc, char *argv[])
  for each connection.  It handles all communication
  once a connnection has been established.
  *****************************************/
-void dostuff (int sock)
+void dostuff( int sock )
 {
-   int n;
-   char buffer[256];
+    int n;
+    char buffer[ 256 ];
       
-   bzero(buffer,256);
-   n = read(sock,buffer,255);
-   if (n < 0) error("ERROR reading from socket");
-   printf("Here is the message: %s\n",buffer);
-   n = write(sock,"I got your message",18);
-   if (n < 0) error("ERROR writing to socket");
+    bzero( buffer, 256 );
+    n = read( sock, buffer, 255 );
+    if( n < 0 )
+    {
+        error( "ERROR reading from socket" );
+    }
+    printf( "Here is the message: %s\n", buffer );
+    n = write( sock, "I got your message", 18 );
+    if( n < 0 )
+    {
+        error( "ERROR writing to socket" );
+    }
 }
+
